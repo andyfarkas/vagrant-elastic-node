d96fa65220499b1eb10aab94c80b1e8ca1b23c60
Andrej Farkas <andrej.farkas@dixonsretail.com>
2015-07-14 16:17:55 +0200

    - added php and mongo

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure(2) do |config|
   # config.vm.synced_folder "../data", "/vagrant_data"
 
   config.vm.provider "virtualbox" do |vb|
-     vb.memory = "1024"	 
+     vb.memory = "2048"	 
   end
   
   config.vm.provision "shell", path: "provision.sh"
--- provision.sh
+++ provision.sh
@@ -25,3 +25,15 @@ sudo apt-get install -y nodejs
 
 #install nodemon
 sudo npm install -g nodemon
+
+#install php
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+sudo apt-get install -y libapache2-mod-php5
+sudo /etc/init.d/apache2 restart
+
+#install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
